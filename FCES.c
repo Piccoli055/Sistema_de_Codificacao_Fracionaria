92b530bc5b90a476c8a7276a3208f9001571bbdc
Piccoli055 <eduardohpiccoli05@gmail.com>
2026-06-06 21:54:07 -0300

    Add decryption function and multi-word support

--- FCES.c
+++ FCES.c
@@ -33,6 +33,13 @@ void criptografar(char texto[]) {
     printf("Cifra: ");
 
     for (i = 0; i < strlen(texto); i++) {
+        // agora suporta espaco entre palavras
+        if (texto[i] == ' ') {
+            printf(" ");
+            primeiro = 1;
+            continue;
+        }
+
         if (!isalpha(texto[i])) continue;
 
         int x = letraPraNumero(texto[i]);
@@ -52,10 +59,54 @@ void criptografar(char texto[]) {
     printf("\n");
 }
 
+// Agora com descriptografia tambem
+void descriptografar(char cifra[]) {
+    int i = 0;
+
+    printf("Texto: ");
+
+    while (i < strlen(cifra)) {
+        if (cifra[i] == ' ') {
+            printf(" ");
+            i++;
+            continue;
+        }
+
+        int num = 0, den = 0;
+        while (i < strlen(cifra) && cifra[i] != '/') {
+            num = num * 10 + (cifra[i] - '0');
+            i++;
+        }
+        i++; // pula a barra /
+        while (i < strlen(cifra) && cifra[i] != '|' && cifra[i] != ' ') {
+            den = den * 10 + (cifra[i] - '0');
+            i++;
+        }
+        if (cifra[i] == '|') i++; // pula a barra reta |
+
+        int resultado_num = num * B * D;
+        int resultado_den = den * A;
+
+        int d = mdc(resultado_num, resultado_den);
+        int x = resultado_num / d / (resultado_den / d);
+
+        printf("%c", numeroPraLetra(x));
+    }
+
+    printf("\n");
+}
+
 int main() {
-    // Teste direto, sem menu ainda
-    printf("Testando com ZACARIAS:\n");
-    criptografar("ZACARIAS");
+    char cifra[500];
+
+    printf("Criptografando OLA MUNDO:\n");
+    criptografar("OLA MUNDO");
+
+    printf("\nDescriptografando a cifra:\n");
+    printf("Digite a cifra: ");
+    fgets(cifra, sizeof(cifra), stdin);
+    cifra[strcspn(cifra, "\n")] = '\0';
+    descriptografar(cifra);
 
     return 0;
 }
